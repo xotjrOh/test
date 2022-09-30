f8297f237967edf7587cfb9f885e31b6204be116
taeseokOh <xotjr8052@gmail.com>
2022-09-30 17:45:18 +0900

    2480

--- exam.py
+++ exam.py
@@ -1,3 +1,5 @@
-h,m=map(int,input().split())
-k=int(input())
-print((h+(m+k)//60)%24,(m+k)%60)
+a=[*map(int,input().split())]
+k=len(set(a))
+if k == 1:print(a[0]*1000+10000)
+elif k == 2:print(sorted(a)[1]*100+1000)
+else:print(max(a)*100)

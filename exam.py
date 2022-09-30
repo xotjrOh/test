add9158a1b0c349a6009d85fd7c7138214032a0b
taeseokOh <xotjr8052@gmail.com>
2022-09-30 18:46:24 +0900

    2108

--- exam.py
+++ exam.py
@@ -1,3 +1,12 @@
-a,b=map(int,input().split())
-c=sorted(map(int,input().split()))
-print(c[-b])
+from collections import Counter
+import sys
+a=[]
+for _ in " "*int(input()):
+    a+=[int(sys.stdin.readline())]
+a=sorted(a)
+print(round(sum(a)/len(a)))
+print(a[len(a)//2])
+b=Counter(a).most_common(2)
+if len(b)>1 and b[0][1]==b[1][1]:print(b[1][0])
+else : print(b[0][0])
+print(a[-1]-a[0])
